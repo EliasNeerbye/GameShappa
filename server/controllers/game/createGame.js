a7b751777abe150be1c1f9caa61ee549a39fb217
EliasNeerbye <eliasnee@afk.no>
2025-01-20 11:34:49 +0100

    Made create game and get games

--- server/controllers/game/createGame.js
+++ server/controllers/game/createGame.js
@@ -2,22 +2,27 @@
 const Game = require("../../models/Game");
 
 const postGame = async (req, res) => {
-    const { title, originalPrice, description, shortDescription, publisher, developer, releaseDate, status } = req.body;
+    try {
+        const { title, originalPrice, description, shortDescription, publisher, developer, releaseDate, status } = req.body;
 
-    const newGame = new Game({
-        title,
-        originalPrice,
-        description,
-        shortDescription,
-        publisher,
-        developer,
-        releaseDate,
-        status,
-    });
+        const newGame = new Game({
+            title,
+            originalPrice,
+            description,
+            shortDescription,
+            publisher,
+            developer,
+            releaseDate,
+            status,
+        });
 
-    console.log(newGame);
+        console.log(newGame);
 
-    res.send("Cool");
+        await newGame.save();
+        res.status(201).json({ message: "Game created successfully", success: true });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", success: false });
+    }
 };
 
 module.exports = postGame;
--- server/controllers/game/getGames.js
+++ server/controllers/game/getGames.js
@@ -1,5 +1,13 @@
+// Models
+const Game = require("../../models/Game");
+
 const getGame = async (req, res) => {
-    res.send("Hello Game");
+    const games = await Game.find();
+    if (!games) {
+        res.status(404).json({ message: "No games found", success: false, games: null });
+    }
+
+    return res.status(200).json({ message: "Games found!", success: true, games });
 };
 
 module.exports = getGame;
