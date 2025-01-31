5b7ff4991d4df12546b7dd71d4ea10d1c7ad183a
EliasNeerbye <eliasnee@afk.no>
2025-01-31 14:15:07 +0100

    Updates

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import "../css/components/NavBar.css";
+
+export default function NavBar() {
+    return (
+        <>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Shop</a>
+                <a href="#">Login</a>
+            </nav>
+        </>
+    );
+}
---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+import "../css/pages/LandingPage.css";
+
+export default function LandingPage() {
+    return (
+        <>
+            <h1>Welcome</h1>
+        </>
+    );
+}
