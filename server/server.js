a939cbb31bf85eb0f21f1a43bf4d3306aa1e8b4c
EliasNeerbye <eliasnee@afk.no>
2025-02-14 13:33:05 +0100

    Test

--- server/server.js
+++ server/server.js
@@ -9,26 +9,19 @@ mongoose.connect(process.env.MONGODB_URI).then(console.log(`Connected to mongodb
 
 const corsOptions = {
     origin: function (origin, callback) {
-        const allowedOrigins = [
-            "http://gameshappa.caracal.ikt-fag.no",
-            "http://gameshappaserver.caracal.ikt-fag.no",
-            "https://gameshappa.caracal.ikt-fag.no",
-            "http://10.12.45.82",
-            "http://localhost:5173",
-        ];
+        const allowedOrigins = ["http://gameshappa.caracal.ikt-fag.no", "http://localhost:5173"];
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
-            callback(new Error("CORS not allowed"));
+            callback("CORS not allowed");
         }
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin"],
-    exposedHeaders: ["Set-Cookie"],
+    exposedHeaders: ["Set-Cookie", "Content-Range", "X-Content-Range"],
     maxAge: 86400,
 };
-
 
 
 // Enable pre-flight requests for all routes
--- server/util/cookie.js
+++ server/util/cookie.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 
 const day = 1000 * 60 * 60 * 24;
+const maxAge = day * 7;
+
+let secure = false;
+if (process.env.ENVIRONMENT === "production") {
+    secure = true;
+}
 
 const createCookie = async (res, name, cookie) => {
     try {
         res.cookie(name, cookie, {
             httpOnly: true,
-            maxAge: day * 7,
-            secure: process.env.ENVIRONMENT === "production",
-            sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax",
-            domain: process.env.ENVIRONMENT === "production" ? "gameshappa.caracal.ikt-fag.no" : undefined,
+            maxAge,
+            secure,
+            sameSite: "lax",
         });
         return true;
     } catch (err) {
@@ -17,6 +22,5 @@ const createCookie = async (res, name, cookie) => {
         return false;
     }
 };
-
 
 module.exports = createCookie;
